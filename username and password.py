8df1be2f554620187b7471a9b84c03b33e1c28b2
Ismail <165809888+Ismafreelance@users.noreply.github.com>
2024-07-19 17:46:11 +0200

    Add files via upload

---
+++ username and password.py
@@ -0,0 +1,46 @@
+# username and password authentication
+# code 100% written by me
+# My social media:
+# x.com/ismafreelance
+# github.com/ismafreelance
+# instagram.com/ismafreelance
+# youtube.com/@ismafreelance
+
+print("Code written by ismafreelance")
+print("")
+
+#define username
+username = str(input("Username: "))
+
+#confirm username
+while (str(input("Confirm username: "))) != username:
+    print("Usernames do not match")
+    #in case it fails it makes the user repeat it twice for extra security
+    str(input("Confirm username: "))
+    print("Repeat again for extra security")
+#defines de password
+password = str(input("Password: "))
+while (str(input("Confirm password: "))) != password:
+    print("Paswords do not match")
+    #in case it fails it makes the user repeat it twice for extra security
+    str(input("Confirm password: "))
+    print("Repeat again for extra security")
+#when sign up is succesful it prints the next message
+print("User succesfully signed")
+print("")
+#the next code makes the user log in with his new account
+print("Log in")
+#it makes the user enter his username
+usernameguess = str(input("Username: "))
+#in case the user writes the username wrong it makes it try again
+while (str(usernameguess)) != username:
+    print("This username does not exist", "Try again")
+    usernameguess = str(input("Username: "))
+#it makes the user input his password
+passwordguess = str(input("Password: "))
+#in case the user writes the password wrong it makes it try again
+while (str(passwordguess)) != password:
+    print("Wrong password", "Try again")
+    passwordguess = str(input("Password: "))
+#if the user writes the password right it print the next message
+print("Log in succesful")
